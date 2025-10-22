f59f27ecbe6a249a7bc1a0c068253920eb824dfa
whynotbruhh <abhiramchowdary2006@gmail.com>
2025-10-22 23:08:38 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-from fastapi import FastAPI
-from endpoints import router as api_router
-
-app = FastAPI(title="DSA TutorBot API")
-
-# Include all endpoints
-app.include_router(api_router)
